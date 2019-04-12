90242c244071d6c65e3651201101ddf53ddabca9
冬冬 李 <lsongzhi@163.com>
2019-04-12 18:19:58 +0800

    environment

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from vm import VirtualMachine
 
 if __name__ == "__main__":
     expr = Add(Mul(Num(1), Num(2)), Mul(Num(3), Num(4)))
-    vm = VirtualMachine(expr)
+    vm = VirtualMachine(expr, {})
 
     vm.run()
--- vm.py
+++ vm.py
@@ -2,11 +2,12 @@ from syntax.ast import *
 
 
 class VirtualMachine:
-    def __init__(self, expr: Expr):
+    def __init__(self, expr: Expr, env: dict):
         self.expr = expr
+        self.env = env
 
     def step(self):
-        self.expr = self.expr.reduce()
+        self.expr = self.expr.reduce(env=self.env)
 
     def run(self):
         while self.expr.is_reducible:
